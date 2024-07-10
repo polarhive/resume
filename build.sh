7c7d007ac3ce931c2d6e3df2358680c88fe1dbc0
Nathan Paul <mail@polarhive.net>
2024-07-10 19:56:30 +0530

    talks/anna: Go BLR meetup

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 xdg-open résumé.pdf &
-inotifywait -m -e modify résumé.tex | while read -r filename event; do
-    echo "Detected change in $filename. Running build script..."
-    tectonic résumé.tex --keep-intermediates
-done
 cp résumé.pdf ~/.local/repos/site/static/assets/résumé.pdf
+inotifywait -m -e modify résumé.tex | while read -r filename event; do
+  tectonic résumé.tex --keep-intermediates
+done
