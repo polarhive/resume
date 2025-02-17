d85e04d194e75e9a89d89365fc0e90908fded858
Nathan Paul <mail@polarhive.net>
2025-02-18 01:56:35 +0530

    chore: update

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-xdg-open résumé.pdf &
-cp résumé.pdf ~/.local/repos/site/static/assets/résumé.pdf
+tectonic résumé.tex --keep-intermediates && xdg-open résumé.pdf &
 inotifywait -m -e modify résumé.tex | while read -r filename event; do
   tectonic résumé.tex --keep-intermediates
 done
